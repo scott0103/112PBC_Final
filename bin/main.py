439344ee08c893080ac95d24e53889482eb8bdd5
Rinn <a0970334118@gmail.com>
2023-05-25 19:51:30 +0800

    5/25 8. update

--- bin/main.py
+++ bin/main.py
@@ -10,17 +10,23 @@ class Calender_main(tk.Frame):
     self.creatWidget()
   def createDays(self):
     font1 = tkFont.Font(size = 18, family = 'Times New Roman')
-    column_weekdays = tk.Label(self, text = '一 二 三 四 五 六 日', height = 1,\
-                               width = 16, font = font1)   
-    column_weekdays.grid(row = 0, column = 1, columnspan = 7)
+    for i in range(1, 8):
+      column_weekdays = tk.Label(self, text = str(i), height = 1,\
+                                 width = 1, font = font1)   
+      column_weekdays.grid(row = 0, column = i)
   def createTimes(self):
     font2 = tkFont.Font(size = 18, family = 'Times New Roman')
     row_times = tk.Label(self, text = '8.10', font = font2)
     row_times.grid(row = 1,column = 0)  
   def creatWidget(self):
     font3 = tkFont.Font(size = 18, family = 'Times New Roman')
-    workAppend = tk.Button(self, text = '[]',height = 3, width = 9,font = font3)
-    workAppend.grid(row = 1, column = 1)
+    for i in range(1, 8):
+      for a in range(1, 9):
+        workAppend = tk.Button(self, text = '[]',height = 1, width = 4,font = font3)
+        workAppend.grid(row = a, column = i)
+  #def creatButton(self):
+  #def widgetCommand(self):
+    
 cal = Calender_main()
 cal.master.title('日歷')
 cal.mainloop()
