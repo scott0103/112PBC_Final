928ce795267a66fc519d4afb06c05fa8979480db
Rinn <a0970334118@gmail.com>
2023-05-25 19:07:49 +0800

    5/25 7.04 update

--- 112PBC/main.py
+++ 112PBC/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as tkFont
 
 class Calender_main(tk.Frame):
   def __init__(self):
@@ -6,12 +7,20 @@ class Calender_main(tk.Frame):
     self.grid()
     self.createDays()
     self.createTimes() 
+    self.creatWidget()
   def createDays(self):
-    column_weekdays = tk.Label(self, text = '一 二 三 四 五 六 日')
-    column_weekdays.grid(row = 0,column = 1)
+    font1 = tkFont.Font(size = 18, family = 'Times New Roman')
+    column_weekdays = tk.Label(self, text = '一 二 三 四 五 六 日', height = 1,\
+                               width = 16, font = font1)   
+    column_weekdays.grid(row = 0, column = 1, columnspan = 7)
   def createTimes(self):
-    row_times = tk.Label(self, text = '8.10')
+    font2 = tkFont.Font(size = 18, family = 'Times New Roman')
+    row_times = tk.Label(self, text = '8.10', font = font2)
     row_times.grid(row = 1,column = 0)  
+  def creatWidget(self):
+    font3 = tkFont.Font(size = 18, family = 'Times New Roman')
+    workAppend = tk.Button(self, text = '[]',height = 3, width = 9,font = font3)
+    workAppend.grid(row = 1, column = 1)
 cal = Calender_main()
 cal.master.title('日歷')
 cal.mainloop()
